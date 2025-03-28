81d31cfb3ccd6e79e64e041717fcc9d4fce60ac8
LalauSenai <pedrootaviolalaurocha@gmail.com>
2025-03-28 15:52:23 -0300

    commit final do dia 28.03

--- app.js
+++ app.js
@@ -1,24 +1,31 @@
 const express = require("express");
 const sqlite3 = require("sqlite3");
+const bodyParser = require ("body-parser");
 
 const PORT = 8000; // Porta TCP do servidor HTTP da aplicação
 
 const app = express();
 
 const db = new sqlite3.Database("user.db");
+
 db.serialize( () => {
-    db.run(
-        "CREATE TABLE IF NOT EXISTS users (id INTEGER PRIMARY KEY AUTOINCREMENT, username TEXT, password TEXT)"
-    );
+    db.run(`CREATE TABLE IF NOT EXISTS users (id INTEGER PRIMARY KEY AUTOINCREMENT, 
+         username TEXT, password TEXT, email TEXT, celular TEXT, cpf TEXT, rg TEXT)`);
 });
 
-const index= "<a href='/sobre'>Sobre</a><a href='/info'>Info</a>";
-const sobre = 'Vc está na pagina "Sobre"<br><a href="/">Voltar</a>';
-const login = 'Vc está na pagina "Login"<br><a href="/">Voltar</a>';
+app.use("/static", express.static(__dirname + "/static"));
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.set("view engine", "ejs");
+
+//nst index= "<a href='/sobre'>Sobre</a><a href='/info'>Info</a>";
+// const sobre = 'Vc está na pagina "Sobre"<br><a href="/">Voltar</a>';
+// const login = 'Vc está na pagina "Login"<br><a href="/">Voltar</a>';
 const cadastro = 'Vc está na pagina "Casdatro"<br><a href="/">Voltar</a>';
 
 app.get('/', (req, res) => {
-    res.send(index);
+    res.render("index");
 });
 
 app.get("/sobre",(req,res) => {
@@ -29,11 +36,26 @@ app.get("/login",(req,res) => {
     res.send(login);
 })
 
+app.post("/login",(req, res) => {
+    res.send("Login ainda não implementado.");
+});
+
+
 app.get("/cadastro",(req,res) => {
     res.send(cadastro);
-})
+});
+
+app.post("/cadastro",(req,res) => {
+    req.body
+    ? console.log(JSON.stringify(req.body))
+    : console.log(`Body vazio: ${req.body}`);
+    res.send (
+        `Bem vindo usuario: ${req.body.username}, seu email é ${req.body.email}`
+    );
+});
+
+
 // app.listen() deve ser o último comando da aplicação (app.js)
 app.listen(PORT, () =>{
     console.log(`Servidor sendo executado na porta ${PORT}!`);
 });
-
